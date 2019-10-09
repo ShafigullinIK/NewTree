1991e9244ba540fcb5b2e8d65932f9506df5d30c
ShafigullinIK <44900148+ShafigullinIK@users.noreply.github.com>
2019-10-09 17:13:45 +0300

    printTree() - created;

--- MyTree.java
+++ MyTree.java
@@ -7,22 +7,17 @@ public class MyTree {
     }
 
     public void add(int number){
-        add(root, number);
+        add(root, number + "");
     }
 
-    public void add(Item item, int number){
-        if(number == -1){
+    public void add(Item item, String number){
+        if("".equals(number)){
             item.setEnd(true);
 
         } else {
-            String s = number + "";
-            int a = s.charAt(0) - 48;
-            int result = -1;
-            try{
-                result = Integer.parseInt(s.substring(1));
-            } catch (Exception e) {
+            int a = number.charAt(0) - 48;
+            String result = number.substring(1);
 
-            }
             Item[] connections = item.getConnections();
             if(connections[a] == null){
                 Item newItem = new Item(a);
@@ -32,7 +27,49 @@ public class MyTree {
                 add(connections[a], result);
             }
         }
-        System.out.println("finish");
+    }
+
+    public boolean findInt(int value){
+        return  false; // метод не закончен
+    }
+
+    public void deleteInt(int value){
+        // если это не последний элемент дерева
+        // то его связи остаются, он просто не End
+        // Если он последний, то удаляется и связь
+        // с этим элементом.
+    }
+
+    public void printSubStree(int startingValue){
+        // проверить есть ли такой старт в дереве
+        // Если есть, то вывести все элементы,
+        // которые начинаются с этого набора цифр.
+    }
+
+    public void printTree(){
+        String s = "";
+        printTree(root, s);
+    }
+
+    private void printTree(Item item, String s){
+        Item[] connections = item.getConnections();
+        if(item.getValue() != -1){
+            s+=item.getValue() + "";
+        }
+        System.out.println("storage in = " + s);
+        if(item.isEnd() == true){
+            System.out.println("-->" + s);
+        }
+        for(int i = 0; i < connections.length; i++){
+           if(connections[i] != null){
+               printTree(connections[i], s);
+               System.out.println("Storage out = " + s);
+           }
+        }
+
+
+
+
     }
 
 }
